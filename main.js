740a652cce6fdc3fc34930e14e5c79dfedc23e77
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-05-23 13:57:08 +0100

    Initial Upload

---
+++ main.js
@@ -0,0 +1,69 @@
+async function run() {
+	let row1HTML = '';
+
+	const mainJSONResponse = await fetch('main.json');
+	if (!mainJSONResponse.ok) {
+		throw new Error(`Response status: ${mainJSONResponse.status}`);
+		return;
+	}
+
+	const mainJSON = await mainJSONResponse.json();
+	console.log(mainJSON);
+
+	for (let p = 0; p < mainJSON.players.length; ++p) {
+		// console.log(mainJSON.players[i]);
+
+		const playerResponse = await fetch(mainJSON.players[p]);
+		if (!playerResponse.ok) {
+			throw new Error(`Response status: ${playerResponse.status}`);
+			continue;
+		}
+		const player = await playerResponse.json();
+		player.dragons.sort(sortDragons);
+		console.log(player);
+
+		let column = `<div class="column"><table>`
+
+		//  ========== HEADER ==========
+		column += `<tr><th>${player.name}</th><th>Count</th><th>View</th></tr>`;
+
+		// ========== DRAGONS ==========
+		for (let d = 0; d < player.dragons.length; ++d) {
+			let row = `<tr>`;
+
+			// Show Eggs
+			row += `<td>`
+			for (let i = 0; i < mainJSON.breeds[player.dragons[d].id].name.length; ++i) {
+				row += `<img src="${mainJSON.breeds[player.dragons[d].id].img[i]}"`;
+				row += `title="${mainJSON.breeds[player.dragons[d].id].name[i]}"> `;
+			} 
+			row += `</td>`;
+
+			// Count
+			row += `<td>${player.dragons[d].view.length}</td>`;
+			
+			// View https://dragcave.net/image/r5HjG.gif
+			row += `<td>`;
+			for (let i = 0; i < player.dragons[d].view.length; ++i) {
+				row += `<img src="https://dragcave.net/image/${player.dragons[d].view[i]}.gif"> `;
+			}
+			row += `</td>`
+
+			// End
+			row += `</tr>`
+
+			column += row;
+		}
+
+		column += `</table></div>`;
+
+		row1HTML += column;
+	}
+	document.getElementById('row1').innerHTML = row1HTML;
+}
+
+function sortDragons(a, b) {
+	if (a.view.length !== b.view.length) return a.view.length - b.view.length;
+
+	return a.id.localeCompare(b.id);
+}
